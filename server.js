34079247f28fe30baab25a597248859f02915717
khosrow36 <artek.przystas@gmail.com>
2020-09-09 19:58:32 +0200

    added colors to console logs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const connectDB = require('./config/db')
+const colors = require('colors')
 
 // Env files
 dotenv.config({path: './config/config.env'});
@@ -25,11 +26,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Hangle ungladled promise rejecstions
 process.on('unhandledRejection', (err, promise) =>{
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     // Close server
     server.close(() => process.exit(1));
 })
