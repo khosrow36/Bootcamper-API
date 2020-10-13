e6ac86264ba1c07017d3a8d8d81aecc218267bec
khosrow36 <artek.przystas@gmail.com>
2020-10-13 23:48:39 +0200

    added bootcamps & users relation

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,7 +8,6 @@ const geocoder = require('../utils/geocoder')
 // @route   GET /api/api/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-
     res.status(200).json(res.advacentResults);
 });
 
@@ -30,6 +29,14 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/api/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp & if user can add another bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({user: req.user.id});
+    if(publishedBootcamp && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`The user with id ${req.user.id} has already published a bootcamp`), 400);
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({
         success: true,
