29cabddbb74b97cd3be14ce3ad9f79f7109fb36c
khosrow36 <artek.przystas@gmail.com>
2020-10-14 22:54:04 +0200

    Update User Data

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,43 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     })
 });
 
+// @desc    Update user details
+// @route   PUT /api/api/auth/updatedetails
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+});
+
+// @desc    Update password
+// @route   PUT /api/api/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    if(!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Password incorrect', 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
+
+
 // @desc    Forgot Password
 // @route   POST /api/api/auth/forgotpassword
 // @access  Private
