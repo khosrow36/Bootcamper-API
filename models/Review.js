2cf475db71b8ff6c4cfcbc6f3fa79f30e386601e
khosrow36 <artek.przystas@gmail.com>
2020-10-15 21:10:19 +0200

    added reviews

---
+++ models/Review.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Add a review title'],
+        maxlength: 100
+    },
+    text: {
+        type: String,
+        required: [true, 'Add some text']
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+        required: [true, 'Add a rating between 1 and 10']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+// Prevent user from submitting more than one review per bootcamp
+ReviewSchema.index({bootcamp: 1, user: 1}, {unique: true});
+
+// static method to get avg of course rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: {$avg: '$rating'}
+            }
+        }
+    ]);
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// Call getAverage cost after save
+ReviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverage cost before remove
+ReviewSchema.pre('delete', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
