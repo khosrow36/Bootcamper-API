b80233e3c12fc358f24495df82f546be16c7c8c6
khosrow36 <artek.przystas@gmail.com>
2020-10-20 23:29:18 +0200

    added xss prevention and security headers

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 // Env files
 dotenv.config({path: './config/config.env'});
@@ -41,6 +43,12 @@ app.use(fileupload());
 
 // Sanitaze data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
